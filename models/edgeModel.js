5ececebab78452e231c3e883172a4472e0e6be1f
soongfs <1415022760@qq.com>
2025-06-02 21:55:11 +0800

    路径规划策略debug

--- models/edgeModel.js
+++ models/edgeModel.js
@@ -2,7 +2,7 @@
 const db = require("../config/dbConfig");
 
 async function getAllEdges() {
-  const [rows] = await db.query("SELECT from_node, to_node, length FROM edges");
+  const [rows] = await db.query("SELECT from_node, to_node, length, congestion, isCycleable FROM edges");
   return rows;
 }
 
